2c8ef158e4423ea29326947cdb0878c8d3f6ddfd
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2025-05-08 00:24:03 -0300

    Criação de uma interface Simples para listagem das salas.

---
+++ FSBR_AgendaSalas.MVC/Controllers/SalaController.cs
@@ -0,0 +1,31 @@
+﻿using FSBR_AgendaSalas.MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FSBR_AgendaSalas.MVC.Controllers
+{
+    public class SalaController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public SalaController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+            
+            var response = await _httpClient.GetAsync("https://localhost:7081/api/sala"); 
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var salas = JsonConvert.DeserializeObject<List<SalaViewModel>>(content);
+                return View(salas);
+            }
+
+            // Caso a API não retorne sucesso
+            return View(new List<SalaViewModel>());
+        }
+    }
+}
---
+++ FSBR_AgendaSalas.MVC/ViewModels/SalaViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FSBR_AgendaSalas.MVC.ViewModels
+{
+    public class SalaViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Capacidade { get; set; }
+    }
+}
